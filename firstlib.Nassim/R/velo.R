5b5f852db89d43ae55c8e03194ceabad1feca5ff
Nassim <ikhelef.nassim@gmail.com>
2026-04-02 14:01:03 +0200

    Ajout du code de la partie 3 et de la documentation

---
+++ firstlib.Nassim/R/velo.R
@@ -0,0 +1,93 @@
+#' Filtrer les anomalies
+#'
+#' @description Conserve uniquement les trajets où aucune anomalie n'est détectée.
+#' @param trajet Un data.frame contenant les données vélos.
+#' @return Un data.frame filtré.
+#' @importFrom dplyr filter
+#' @export
+filtre_anomalie <- function(trajet){
+  trajet |>
+    filter(is.na(`Probabilité de présence d'anomalies`))
+}
+
+#' Compter le nombre de trajets
+#'
+#' @description Calcule le total des trajets dans le jeu de données.
+#' @param trajet Un data.frame contenant une colonne 'Total'.
+#' @return La somme totale des trajets.
+#' @importFrom dplyr pull
+#' @export
+compter_nombre_trajets <- function(trajet){
+  trajet |>
+    pull(Total) |>
+    sum()
+}
+
+#' Compter le nombre de boucles
+#'
+#' @description Calcule le nombre de boucles de comptage uniques.
+#' @param trajet Un data.frame contenant une colonne 'Numéro de boucle'.
+#' @return Le nombre de boucles distinctes.
+#' @importFrom dplyr pull n_distinct
+#' @export
+compter_nombre_boucle <- function(trajet){
+  trajet |>
+    pull(`Numéro de boucle`) |>
+    n_distinct()
+}
+
+#' Trouver le trajet maximum
+#'
+#' @description Isole la ligne comportant le plus grand nombre de trajets.
+#' @param trajet Un data.frame contenant les colonnes 'Total', 'Jour' et 'Boucle de comptage'.
+#' @return Un data.frame d'une ligne avec le record.
+#' @importFrom dplyr slice_max select
+#' @export
+trouver_trajet_max <- function(trajet){
+  trajet |>
+    slice_max(Total) |>
+    select(`Boucle de comptage`, Jour, Total)
+}
+
+#' Calculer la distribution par semaine
+#'
+#' @description Groupe et additionne les trajets par jour de la semaine.
+#' @param trajet Un data.frame contenant une colonne 'Jour de la semaine'.
+#' @return Un data.frame avec le total par jour.
+#' @importFrom dplyr count
+#' @export
+calcul_distribution_semaine <- function(trajet){
+  trajet |>
+    count(`Jour de la semaine`, wt = Total, sort = TRUE, name = "trajets")
+}
+
+#' Tracer la distribution par semaine
+#'
+#' @description Génère un graphique en barres de la fréquentation par jour.
+#' @param trajet Un data.frame de trajets bruts.
+#' @return Un graphique ggplot.
+#' @importFrom dplyr mutate
+#' @importFrom forcats fct_recode
+#' @importFrom ggplot2 ggplot aes geom_col
+#' @export
+plot_distribution_semaine <- function(trajet) {
+  trajet_weekday <- trajet |>
+    filtre_anomalie() |>
+    calcul_distribution_semaine() |>
+    mutate(
+      jour = fct_recode(
+        factor(`Jour de la semaine`),
+        "lundi" = "1",
+        "mardi" = "2",
+        "mercredi" = "3",
+        "jeudi" = "4",
+        "vendredi" = "5",
+        "samedi" = "6",
+        "dimanche" = "7"
+      )
+    )
+
+  ggplot(trajet_weekday) +
+    aes(x = jour, y = trajets) +
+    geom_col()
+}
